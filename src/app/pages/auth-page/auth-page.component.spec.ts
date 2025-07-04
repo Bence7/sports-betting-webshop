6528bfbfce6516ad8c10bf2b76b1a480778facba
Bence7 <jeneib13@gmail.com>
2025-07-04 20:06:32 +0000

    Fix test

--- src/app/pages/auth-page/auth-page.component.spec.ts
+++ src/app/pages/auth-page/auth-page.component.spec.ts
@@ -1,13 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AuthPageComponent } from './auth-page.component';
 import { provideStore } from '@ngrx/store';
-import { getAuth, provideAuth } from '@angular/fire/auth';
+import { Auth } from '@angular/fire/auth';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../../../environments/environment';
 
 describe('AuthPageComponent', () => {
   let component: AuthPageComponent;
   let fixture: ComponentFixture<AuthPageComponent>;
+  const mockAuth: Partial<Auth> = {};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,7 +16,10 @@ describe('AuthPageComponent', () => {
       providers: [
         provideStore(),
         provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-        provideAuth(() => getAuth()),
+        {
+          provide: Auth,
+          useValue: mockAuth,
+        },
       ],
     }).compileComponents();
 
--- src/app/shared/services/auth/auth.service.spec.ts
+++ src/app/shared/services/auth/auth.service.spec.ts
@@ -1,18 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
-import { provideAuth, getAuth } from '@angular/fire/auth';
+import { Auth } from '@angular/fire/auth';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { environment } from '../../../../environments/environment';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const mockAuth: Partial<Auth> = {};
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-        provideAuth(() => getAuth()),
+        { provide: Auth, useValue: mockAuth },
       ],
     });
     service = TestBed.inject(AuthService);
